7f973f149ec975292aaef1a6005b0030826eec81
angelgame <1759279542@qq.com>
2018-08-26 11:01:12 +0800

    debug

--- script/MemLog.sh
+++ script/MemLog.sh
@@ -1,4 +1,5 @@
-#!/bin/bashi 
+#!/bin/bash 
+DyAver=$1
 MemValue=(`free -m | head -n 2 | tail -n 1 | awk '{printf("%s %s\n",$2,$3)}'`)
 MemAvaPrec=`echo "scale=1;${MemValue[1]}*100/${MemValue[0]}" | bc`
 NowAver=`echo "scale=1;0.8*${MemAvaPrec}+0.2*${DyAver}" | bc`
